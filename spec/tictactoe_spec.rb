4a8dd67a6415bc4c03d7b2f013ff84c3ff70dc9e
Sophia Kim <mail@fifikim.com>
2022-05-10 12:34:48 -0400

    delete extra describe block from tictactoe_spec

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,10 +4,8 @@ require 'rspec'
 require 'tictactoe'
 
 describe 'Tic Tac Toe game' do
-  describe 'TicTacToe' do
-    it 'initializes a new game' do
-      tictactoe = TicTacToe.new
-      expect(tictactoe).to be_instance_of(TicTacToe)
-    end
+  it 'initializes a new game' do
+    tictactoe = TicTacToe.new
+    expect(tictactoe).to be_instance_of(TicTacToe)
   end
 end
