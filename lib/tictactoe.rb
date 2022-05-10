01605e3a031ad2ff1ef9f2d0e292a9f21229d793
Sophia Kim <mail@fifikim.com>
2022-05-10 16:18:08 -0400

    add display_board method to TicTacToe

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -5,5 +5,9 @@ class TicTacToe
     puts "\n\nTIC TAC TOE\n\nInstructions:\nPlayer 1 moves first and marks spaces with an \"X\". Player 2 marks with an \"O\".\n\nEnter the number (1-9) corresponding to the space that you want to mark.\n\nTo win, mark off 3 adjacent spaces (horizontal, vertical, or diagonal) with your marker.\n\nIf there are no available spaces and no player has won, the game will end in a draw.\n\nStarting new game...\n"
   end
 
+  def display_board
+    puts "\n\n 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n"
+  end
+
 end
 
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -13,6 +13,10 @@ describe 'Tic Tac Toe' do
       expect { @tictactoe.display_instructions }.to output("\n\nTIC TAC TOE\n\nInstructions:\nPlayer 1 moves first and marks spaces with an \"X\". Player 2 marks with an \"O\".\n\nEnter the number (1-9) corresponding to the space that you want to mark.\n\nTo win, mark off 3 adjacent spaces (horizontal, vertical, or diagonal) with your marker.\n\nIf there are no available spaces and no player has won, the game will end in a draw.\n\nStarting new game...\n").to_stdout
     end
 
+    it 'displays a numbered board' do
+      expect { @tictactoe.display_board }.to output("\n\n 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    end
+
   end
 
 end
