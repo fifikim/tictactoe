83da79964e788ebd86c2d753b20ab3b571ae5f20
Sophia Kim <mail@fifikim.com>
2022-05-10 16:17:22 -0400

    add display_instructions method to TicTacToe

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class TicTacToe
-  puts 'hello world'
+  def display_instructions 
+    puts "\n\nTIC TAC TOE\n\nInstructions:\nPlayer 1 moves first and marks spaces with an \"X\". Player 2 marks with an \"O\".\n\nEnter the number (1-9) corresponding to the space that you want to mark.\n\nTo win, mark off 3 adjacent spaces (horizontal, vertical, or diagonal) with your marker.\n\nIf there are no available spaces and no player has won, the game will end in a draw.\n\nStarting new game...\n"
+  end
+
 end
+
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-require 'rspec'
 require 'tictactoe'
 
-describe 'Tic Tac Toe game' do
-  it 'initializes a new game' do
-    tictactoe = TicTacToe.new
-    expect(tictactoe).to be_instance_of(TicTacToe)
+describe 'Tic Tac Toe' do
+
+  context 'when starting a new game' do
+    before do
+      @tictactoe = TicTacToe.new
+    end
+
+    it 'displays instructions' do
+      expect { @tictactoe.display_instructions }.to output("\n\nTIC TAC TOE\n\nInstructions:\nPlayer 1 moves first and marks spaces with an \"X\". Player 2 marks with an \"O\".\n\nEnter the number (1-9) corresponding to the space that you want to mark.\n\nTo win, mark off 3 adjacent spaces (horizontal, vertical, or diagonal) with your marker.\n\nIf there are no available spaces and no player has won, the game will end in a draw.\n\nStarting new game...\n").to_stdout
+    end
+
   end
+
 end
