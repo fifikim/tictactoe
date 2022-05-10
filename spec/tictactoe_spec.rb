1a96106be7bab38b0e2bcb2302a921419ba610c6
Sophia Kim <mail@fifikim.com>
2022-05-10 11:01:51 -0400

    update test to clear up Rubocop 'useless assignment' offense

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,7 +4,10 @@ require 'rspec'
 require 'tictactoe'
 
 describe 'Tic Tac Toe game' do
-  it 'initializes a new game' do
-    tictactoe = TicTacToe.new
+  describe 'TicTacToe' do
+    it 'initializes a new game' do
+      tictactoe = TicTacToe.new
+      expect(tictactoe).to be_instance_of(TicTacToe)
+    end
   end
 end
