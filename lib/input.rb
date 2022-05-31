51e53785932d48d84ee8d4de574b6df9fa8af48e
Sophia Kim <mail@fifikim.com>
2022-05-31 11:26:44 -0400

    remove comments accidentally left in

--- lib/input.rb
+++ lib/input.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Input
-  ## self.get_space
   def self.choose_space
     $stdin.gets.strip
   end
 
-  ## self.get_option
   def self.choose_option
     selection = choose_space
     selection.to_i - 1
